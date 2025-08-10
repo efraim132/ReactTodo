a4f403ff69d47f8e7c18257ab684cf3a9416ecc0
efraim132 <efraim.egrebnev.grebnev605@gmail.com>
2025-08-10 15:19:46 -0700

    Update base path in Vite config and fix asset links in index.html for correct deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ReactTodo/', // <-- your repo name
+  base: '/ReactTodo/dist/', // <-- your repo name
   plugins: [react()],
 })
