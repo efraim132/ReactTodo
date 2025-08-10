928fa3e7c91fe86280e7cc1f349dc2fd3d92501b
efraim132 <efraim.egrebnev.grebnev605@gmail.com>
2025-08-10 15:27:12 -0700

    Continuing from the previous push, troubleshooting difficulties with github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/ReactTodo/dist/', // <-- your repo name
+  base: '/ReactTodo/',
   plugins: [react()],
 })
+
